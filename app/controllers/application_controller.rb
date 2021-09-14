c3f2a3f71efdef92aca224d339b643f091642a50
Samantha Laine <samanthalaine8@gmail.com>
2021-09-14 12:26:47 -0400

    user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,5 +18,31 @@ class ApplicationController < Sinatra::Base
     Review.all.to_json
   end
 
+  post '/signup' do
+    if User.find_by(username: user_params['username'])
+      {message: 'Username exists. Please login.'}.to_json
+    else
+      @user = User.create(user_params)
+      @user.to_json
+    end
+end
 
+post '/login' do
+  if User.find_by(username: user_params['username'])
+
+    @user = User.find_by(username: user_params['username'])
+
+    if user.password == user_params['password']
+      { user: @user, message: 'Logged in successfully...'}.to_json
+    end
+
+  else
+    {message: 'User not found. Please sign up'}.to_json
+  end
+
+  private
+  def user_params
+    allowed_params = %w(username password)
+    param.select{ |param, value| allowed_params.include?(param)}
+  end
 end
