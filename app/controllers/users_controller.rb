b8b4898964e1bd147694e1a351cbdc139d7f74c7
Alexis S Liu <syl1350@gmail.com>
2021-09-14 12:22:26 -0400

    added bookshelf route to userbooks and added signup, login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,37 @@
 class UsersController < ApplicationController
 
-get '/users' do
-    User.all.to_json
+# get '/users' do
+#     User.all.to_json
+#   end
+
+post "/signup" do
+  if User.find_by(username: user_params["username"])
+    {message: "username exists. please login..."}.to_json
+  else
+    @user = User.create(user_params)
   end
+end
+
+post "/login" do
+  if User.find_by(username: user_params["username"])
+
+     @user = User.find_by(username: user_params["username"])
+
+      if  @user.password == user_params["password"]
+        { user: @user, message: "logged in"}.to_json
+      else
+        { message: "incorrect! try again "}.to_json
+      end
+
+    else
+      { message: "doesnt exist, please sign up" }.to_json
+  end
+end
+
+private
+def user_params
+  allowed_params = %w(username password)
+  params.select{ |param, value| allowed_params.include?(param)}
+end
 
 end
